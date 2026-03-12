45d4e30abef4466e278083cf9df72abe88e2e131
Grant Delaney <grant.delaney@nscale.com>
2026-03-12 11:50:15 +0000

    Clean up and remove catz

--- src/netbox_plugin_bind_provisioner/service/endpoint/request_handler.py
+++ src/netbox_plugin_bind_provisioner/service/endpoint/request_handler.py
@@ -333,7 +333,8 @@ class DNSBaseRequestHandler(socketserver.BaseRequestHandler):
         logger.info(f"Request from {peer}: key={key_name} resolved to view='{nb_view.name}'")
 
         # Check if catalog zone
-        if dname == "catz" or dname == f"{nb_view.name}.catz":
+        catalog_zone_name = self.server.catalog_zone_map.get(nb_view.name)
+        if catalog_zone_name and dname == catalog_zone_name:
             logger.info(f"Request from {peer}: serving catalog zone '{dname}' for view='{nb_view.name}'")
             zone = catzm.create_zone(dname, nb_view.name)
         else:
